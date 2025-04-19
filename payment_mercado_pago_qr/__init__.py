1eb012fa9c7bd47a0a2bce94525db25c2947d3dd
Martin Quinteros <maq@adhoc.com.ar>
2025-04-18 21:35:40 -0300

    [MIG] payment_mercado_pago_qr: Migration to 17.0

--- payment_mercado_pago_qr/__init__.py
+++ payment_mercado_pago_qr/__init__.py
@@ -4,9 +4,10 @@ from . import wizards
 
 from odoo.addons.payment import setup_provider, reset_payment_provider
 
-def post_init_hook(cr, registry):
-    setup_provider(cr, registry, 'mercado_pago_qr')
+
+def post_init_hook(env):
+    setup_provider(env, "mercado_pago_qr")
 
 
-def uninstall_hook(cr, registry):
-    reset_payment_provider(cr, registry, 'mercado_pago_qr')
+def uninstall_hook(env):
+    reset_payment_provider(env, "mercado_pago_qr")
--- payment_mercado_pago_qr/__manifest__.py
+++ payment_mercado_pago_qr/__manifest__.py
@@ -10,10 +10,11 @@
     "author": "filoquin",
     "website": "http://www.hormigag.ar",
     "category": "sale",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "depends": ["payment"],
     "data": [
         "security/ir.model.access.csv",
+        "data/payment_method_data.xml",
         "data/payment_provider_data.xml",
         "views/payment_provider.xml",
         "views/payment_transaction.xml",
@@ -23,7 +24,7 @@
     "demo": [
         "demo/demo.xml",
     ],
-   'instalable':False,
-   'post_init_hook': 'post_init_hook',
-   'uninstall_hook': 'uninstall_hook',
+    "instalable": False,
+    "post_init_hook": "post_init_hook",
+    "uninstall_hook": "uninstall_hook",
 }
